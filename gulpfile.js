b833f69d1b844225493a99b443c2392618149778
Matt Bevilacqua <mbevilac@providence.edu>
2018-03-14 11:37:51 -0400

    Small Tweak to Lining Up Menu

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,11 @@
 	Open command prompt as administrator
 
 	cd YOUR-THEME-DIRECTORY
-    npm install gulp fs bootstrap@4.0.0-beta.2 postcss-discard-duplicates gulp-autoprefixer gulp-clean gulp-concat gulp-uglify gulp-sourcemaps run-sequence gulp-sass pump gulp-csslint gulp-postcss postcss precss --save-dev
+    npm install
     */
 
 //these are all the modules that you installed above
 var gulp = require('gulp'),
-	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	sourcemaps = require('gulp-sourcemaps'),
@@ -19,9 +18,7 @@ var gulp = require('gulp'),
     cssnano = require('cssnano'),
     discardDuplicates = require('postcss-discard-duplicates'), 
 	postcss = require('gulp-postcss'),
-	transpile  = require('gulp-es6-module-transpiler'),
-	copy = require('copy'),
-	babel = require('gulp-babel');
+	copy = require('copy');
 	
 gulp.task( 'compile-bootstrap', function() { 
 	return gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
@@ -59,17 +56,6 @@ gulp.task('concat-js', function(cb) {
 });
 
 
-// runs all the build processes
-gulp.task('compile', function(){
-	runSequence(
-		'transport-vendor-js',
-		'transport-vendor-css',
-		'build-sass',
-		'concat-js',		
-	);
-});
-
-
 gulp.task( 'transport-vendor-css', function( cb ){
 	copy(['node_modules/normalize.css/normalize.css'], 'css/vendor', {flatten: true}, cb );	
 } );
@@ -82,5 +68,10 @@ gulp.task( 'transport-vendor-js', function( cb ){
 
 // runs all the processes
 gulp.task( 'default', function(){
-	runSequence( 'compile');
+	runSequence(
+		'transport-vendor-js',
+		'transport-vendor-css',
+		'build-sass',
+		'concat-js',		
+	);
 });
